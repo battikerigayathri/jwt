24c5fb4113ff94acaa8dbfa035e9ef9b856f6908
battikerigayathri <145567674+battikerigayathri@users.noreply.github.com>
2023-10-16 11:13:06 +0530

    Update app5.js

--- app5.js
+++ app5.js
@@ -1,87 +1,62 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+const mongoose = require("mongoose");
+const secretkey="thisismyfirstsecret."
 const app = express();
-const port = 2000;
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.json({
+        message: "a simple api"
+        
 
-mongoose.connect("mongodb://localhost:27017/HASHINGMETHOD")
+    })
+})
+mongoose
+  .connect("mongodb://localhost:27017/HASHINGMETHOD")
   .then(() => {
-    console.log({ message: "database connected successfully", status: true });
-  }).catch((error) => {
-    console.log({ message: "failed to connect db", status: false });
-})
-//Define the payload(claims)
-const users = {
-  
-    // Subject
-  name: String,
- password:String,
-};
-const User = [];
-app.post("/register", async (req, res) => {
-  try {
-    const { name, password } = req.body;
-    console.log("REGISTERED SUCCESFULLY");
-    // Generating  a random salt
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    // Hash the password with the salt
-      const hashedPassword = await bcrypt.hash(password, salt);
-   
-    // Store the user in your database
-    const userData = {  name, password: hashedPassword ,salt};
-    User.push(userData)
-      
-  
-
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-app.post("/login", async (req, res) => {
-  try {
-    const { name,  password } = req.body;
-
-    // Finding  the user in your database
-    const users = User.find((u) => u.name === name);
-
-    
-    // Hash the entered password with the user's salt
-    const hashedPassword = await bcrypt.hash(password, users.salt);
-
-    // Compare the hashed password with the stored hashed password
-    if (hashedPassword === users.password) {
-      res.json({ message: "Login successful" });
-    } else {
-      res.status(401).json({ message: " login Authentication failed" });
+    console.log({ message: "databasae connected successfully" });
+  })
+  .catch((error) => {
+    console.log({ messagae: "failed to connect db" });
+  });
+app.post("/login", (req, res) => {
+    const user = {
+        id: 1,
+        username: "john",
+        email: "john123@gmail.com"
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-// Secret key for signing the token (keep it secret)
-const secretKey = "thisismyfirstsecret."; // Replace with your actual secret key
-
-// Generate the JWT
-const token = jwt.sign(users, secretKey);
-
-console.log("Generated Token:", token);
-// Verify and decode the JWT
-jwt.verify(token, secretKey, (err, decoded) => {
-  if (err) {
-    console.error("Token verification failed:", err.message);
-  } else {
-    console.log("Decoded Token:", decoded);
-  }
-});
-app.listen(port, () => {
-    console.log(`server runs at ${port}`);
+    jwt.sign({ user }, secretkey, { expiresIn: '500s' }, (err, token) => {
+         res.json({ token });
+    })
+       
 })
+app.post("/profile", verifyToken, (req, res) => {
+    jwt.verify(req.token, secretkey, (err, authData) => {
+        if (err) {
+            res.send({ result: "invalid token" })
+        } else {
+            res.json({
+                msg: "profile accessed",
+                authData
+            })
+        }
+    })
+})
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    } else {
+        res.send({token:"token is not valid"})
+    }
+    }
+app.listen(4000, () => {
+    console.log("server runs at 4000 port");
+})
+  
+    
+
+     
